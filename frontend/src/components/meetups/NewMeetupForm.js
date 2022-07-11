21ce6f90a1faedb11dc14a34c5ff1ae5d4db87a2
michalbidzinski1 <michal.bidzinski1337@gmail.com>
2022-07-11 20:55:43 +0200

    working post request

--- frontend/src/components/meetups/NewMeetupForm.js
+++ frontend/src/components/meetups/NewMeetupForm.js
@@ -1,42 +1,56 @@
+import { useRef } from "react";
+
 import Card from "../UI/Card";
-import { Field, Form, Formik } from "formik";
 import classes from "./NewMeetupForm.module.css";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-function NewMeetupForm() {
-  const { register, handleSubmit } = useForm();
-  const [meetupData, setData] = useState("");
-  console.log(meetupData);
+
+function NewMeetupForm(props) {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+    };
+
+    props.onAddMeetup(meetupData);
+  }
+
   return (
     <Card>
-      <form
-        className={classes.form}
-        onSubmit={handleSubmit((meetupData) =>
-          setData(JSON.stringify(meetupData))
-        )}
-      >
+      <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="title">Meetup Title</label>
-          <input {...register("title")} type="text" required id="title" />
+          <input type="text" required id="title" ref={titleInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="image">Meetup Image</label>
-          <input {...register("image")} type="url" required id="image" />
+          <input type="url" required id="image" ref={imageInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="address">Address</label>
-          <input {...register("address")} type="text" required id="address" />
+          <input type="text" required id="address" ref={addressInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="description">Description</label>
           <textarea
-            {...register("description")}
             id="description"
             required
             rows="5"
+            ref={descriptionInputRef}
           ></textarea>
         </div>
-
         <div className={classes.actions}>
           <button>Add Meetup</button>
         </div>
@@ -44,4 +58,5 @@ function NewMeetupForm() {
     </Card>
   );
 }
+
 export default NewMeetupForm;
--- frontend/src/pages/NewMeetup.js
+++ frontend/src/pages/NewMeetup.js
@@ -1,10 +1,26 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import { useHistory } from "react-router-dom";
+const axios = require("axios");
 
 function NewMeetupPage() {
+  const history = useHistory();
+
+  const addMeetupHandler = (meetupData) => {
+    axios({
+      method: "post",
+      url: "http://localhost:5000/meetups",
+      data: meetupData,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      console.log(res);
+    });
+  };
   return (
     <section>
       <h1> Add new meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
